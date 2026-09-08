abe94af6b2702716600fdce7b260ef4cc2adc29c
webbingon <271410277+webbingon@users.noreply.github.com>
2026-09-08 21:06:26 +0900

    feat(security): encrypt bot token using safeStorage

--- src/main/store.ts
+++ src/main/store.ts
@@ -1,28 +1,65 @@
+import { app, safeStorage } from 'electron';
 import Store from 'electron-store';
 
 export interface ConfigStoreSchema {
-  appId?: string;
   botToken?: string;
+  isBotTokenEncrypted?: boolean;
 }
 
 export class ConfigStore {
   store: Store;
 
   constructor() {
-    this.store = new Store<ConfigStoreSchema>({
-      encryptionKey: 'your-app-secret-key',
-    });
+    this.store = new Store<ConfigStoreSchema>();
   }
 
   getConfig(): ConfigStoreSchema {
+    const isBotTokenEncrypted = this.store.get('isBotTokenEncrypted', false) as boolean;
+    const botToken = this.store.get('botToken', '') as string;
+
+    if (isBotTokenEncrypted) {
+      if (safeStorage.isEncryptionAvailable()) {
+        try {
+          const encryptedBufferBotToken = Buffer.from(botToken, 'base64');
+          const decryptedBotToken = safeStorage.decryptString(encryptedBufferBotToken);
+          return {
+            botToken: decryptedBotToken,
+          };
+        } catch (err) {
+          throw new Error('トークンの復号に失敗しました。', { cause: err });
+        }
+      } else {
+        console.warn('The bot token is encrypted but safeStorage is unavailable.');
+        return { botToken: '' };
+      }
+    }
+
     return {
-      appId: this.store.get('appId', ''),
-      botToken: this.store.get('botToken', ''),
+      botToken,
     };
   }
 
   saveConfig(config: ConfigStoreSchema): void {
-    this.store.set('appId', config.appId);
-    this.store.set('botToken', config.botToken);
+    if (typeof config.botToken !== 'undefined') this.setBotToken(config.botToken);
+  }
+
+  setBotToken(token: string): void {
+    if (!token) {
+      this.store.delete('botToken');
+      this.store.delete('isBotTokenEncrypted');
+      return;
+    }
+
+    if (safeStorage.isEncryptionAvailable()) {
+      const encryptedBuffer = safeStorage.encryptString(token);
+      this.store.set('botToken', encryptedBuffer.toString('base64'));
+      this.store.set('isBotTokenEncrypted', true);
+    } else if (!app.isPackaged) {
+      console.warn('safeStorage is unavailable.');
+      this.store.set('botToken', token);
+      this.store.set('isBotTokenEncrypted', false);
+    } else {
+      throw new Error('OSの暗号化機能 (safeStorage) が利用できません。');
+    }
   }
 }
